c5af03908ed74dacf5cf693b0493192a3196c3d5
Kamil Kryjak <kamilkryjak95@gmail.com>
2016-01-05 17:19:16 +0100

    Add all files

---
+++ aghInclude.h
@@ -0,0 +1,11 @@
+﻿#ifndef AGH_INCLUDE
+#define AGH_INCLUDE
+// --------------------------------------------------------------
+
+#include "aghContainer.h"
+#include "aghDlist.h"
+#include "aghSlist.h"
+#include "aghException.h"
+// --------------------------------------------------------------
+
+#endif // AGH_INCLUDE
